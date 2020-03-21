fdbe6a3602710e129a96fcb07ffd09def03e2790
nahid940 <nahid940@gmail.com>
2020-03-21 22:35:36 +0600

    Delete function added

--- src/pages/product/product-list.component.jsx
+++ src/pages/product/product-list.component.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect,useState } from 'react'
 import axios from 'axios'
+import alertify from 'alertifyjs'
 
 const ProductList=()=>
 {
@@ -11,11 +12,24 @@ const ProductList=()=>
 		.then(response=>(setProducts(...products,response.data.products)))
 	},[])
 
+	const deleteProduct=(id)=>
+	{
+		alertify.confirm('Alert', 'Do you want to remove this item?', function()
+        {
+			axios.post('http://localhost/ecommerce-api/apis/products.php?remove',
+			{id:id}
+			).then(response=>response.data.status==200?alertify.success(response.data.message):'')
+			const new_product_array=products.filter(product=>product.productID!=id)
+			setProducts(new_product_array)
+		},function(){})
+	}
+
     return(
         <div className="panel panel-flat">
 		<div className="panel-heading">
 			<h5 className="panel-title">Product List<a className="heading-elements-toggle"><i className="icon-more"></i></a></h5>
 		</div>
+	
 
 		<div className="table-responsive">
 			<table className="table">
@@ -33,7 +47,7 @@ const ProductList=()=>
 				</thead>
 				<tbody>
 					{products.map(product=>(
-						<tr>
+						<tr key={product.productID}>
 							<td>{product.productID}</td>
 							<td><img width="60px" src={`http://localhost/ecommerce-api/${product.product_image}`} alt="Image"/></td>
 							<td>{product.product_title}</td>
@@ -44,7 +58,9 @@ const ProductList=()=>
 							<td>
 								<a href=""><i className="icon-pen text-info"></i></a>
 								&nbsp;
-								<a href=""><i className="icon-trash text-danger"></i></a>
+								<a onClick={()=>deleteProduct(product.productID)}>
+									<i className="icon-trash text-danger"></i>
+								</a>
 							</td>
 						</tr>
 					))}
